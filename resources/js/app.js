6a140ab999f9eb518f72e0a68dfee5f367821813
Daniel Burgess <burgess.daniel4141@gmail.com>
2025-11-30 17:39:29 -0500

    Add Ziggy routes file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,24 @@
-import '../css/app.css';
-import './bootstrap';
-import 'vue3-toastify/dist/index.css';
+import '../css/app.css'
+import './bootstrap'
+import 'vue3-toastify/dist/index.css'
 
-import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { createApp, h } from 'vue'
 
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
 import { Ziggy } from './ziggy'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
+console.log('Booting Inertia app', import.meta.env.MODE)
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
   resolve: (name) =>
-    resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolvePageComponent(
+      `./Pages/${name}.vue`,
+      import.meta.glob('./Pages/**/*.vue'),
+    ),
   setup({ el, App, props, plugin }) {
     const vueApp = createApp({ render: () => h(App, props) })
       .use(plugin)
