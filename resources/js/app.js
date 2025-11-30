7d1b09f6912301bffdca726b31585c09fee696df
Daniel Burgess <burgess.daniel4141@gmail.com>
2025-11-30 17:39:03 -0500

    Add Ziggy routes file

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,25 +2,27 @@ import '../css/app.css';
 import './bootstrap';
 import 'vue3-toastify/dist/index.css';
 
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, h } from 'vue';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
+import { Ziggy } from './ziggy'
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-console.log('Booting Inertia app', import.meta.env.MODE);
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
+
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
   resolve: (name) =>
-    resolvePageComponent(
-      `./Pages/${name}.vue`,
-      import.meta.glob('./Pages/**/*.vue'),
-    ),
+    resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
-    const vueApp = createApp({ render: () => h(App, props) }).use(plugin);
-    vueApp.mount(el);
+    const vueApp = createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(ZiggyVue, Ziggy)
+
+    vueApp.mount(el)
   },
   progress: {
     color: '#4B5563',
   },
-});
+})
